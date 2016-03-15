93e8dbcbe3f06abb8ff783b4601a3d946ffb35e2
kowalski100 <ijaz.a100@yahoo.com>
2016-03-15 19:44:09 +0500

    new repo

---
+++ assets/js/custom.js
@@ -0,0 +1,210 @@
+// Window load function .
+(function($){
+	$(window).load(function(){
+		$.mCustomScrollbar.defaults.scrollButtons.enable=true; //enable scrolling buttons by default
+		$.mCustomScrollbar.defaults.axis="yx"; //enable 2 axis scrollbars by default						
+		$("#jstree_demo_div").mCustomScrollbar();// Network operations.
+		$("#Queries_panel").mCustomScrollbar();// Queries_panel
+		$("#parameters_panel").mCustomScrollbar();// Parameters_panel
+		$("#layout-contents").mCustomScrollbar();// Layout contents
+		$("#network_overview_panel").mCustomScrollbar();// Network Overview
+		$("#dynamic_panel").mCustomScrollbar();// Network Overview
+		$(".popup-in").mCustomScrollbar();//  popup-box statistics
+	});
+})(jQuery);	
+// End of Window load function .
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+	
+					$( '.tree li' ).each( function() {
+						if( $( this ).children( 'ul' ).length > 0 ) {
+								$( this ).addClass( 'parent' );     
+						}
+				});
+				
+				$( '.tree li.parent > a' ).click( function( ) {
+						$( this ).parent().toggleClass( 'active' );
+						$( this ).parent().children( 'ul' ).slideToggle( 'fast' );
+				});
+    // Easy tree
+	$('#networkoperations-folder').easytree();
+	
+	// Nicescroll to whole page
+	$("html").niceScroll({
+		touchbehavior:false,
+		horizrailenabled:false,
+		cursorcolor:"#bababa",
+		zindex:9000,
+		cursoropacitymax:1,
+		cursorwidth:7,
+		background:"#1d1d1d",
+		autohidemode:true,
+		cursorborderradius:"0px",
+		cursorborder:"0px",
+		});
+	// Decorating Checkbox
+	$('#collapse3 input').iCheck({
+		radioClass: 'iradio_square-grey',
+		checkboxClass: 'icheckbox_square-grey',
+		increaseArea: '20%' // optional
+   }); // End of checkbox.
+    console.log( "ready!" );
+	$(".user-nav-slide").click(function () {
+		if ($(".user-menu-container").hasClass("closed")) {
+			$(".user-menu-container").animate({"left": "0px"});
+			$(".user-menu-container").removeClass("closed");
+		} else {
+			$(".user-menu-container").animate({"left": "-282px"});
+			$(".user-menu-container").addClass("closed");
+		}
+	});
+		// Right sidebar
+	$(".user-nav-slide-right").click(function () {
+		if ($(".user-menu-container-right").hasClass("closed")) {
+			$(".user-menu-container-right").animate({"right": "0px"});
+			$(".user-menu-container-right").removeClass("closed");
+		} else {
+				$(".user-menu-container-right").animate({"right": "-282px"});
+				$(".user-menu-container-right").addClass("closed");
+		}
+	});
+ /*************************************layout dropdown*********************************************/
+	$(".layout_panel select").change(function(){
+        $(this).find("option:selected").each(function(){
+            if($(this).attr("value")=="rotation"){
+                $(".Rotate").show();
+				$(".Scale").hide();
+				$(".forceAtlas").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".labelAdjust").hide();
+				$(".noverLap").hide();
+				$(".openOrd").hide();
+				$(".randomLayout").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+            else if($(this).attr("value")=="scale"){
+				$(".Scale").show();
+				$(".Rotate").hide();
+				$(".forceAtlas").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".labelAdjust").hide();
+				$(".noverLap").hide();
+				$(".openOrd").hide();
+				$(".randomLayout").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="forceatlas"){
+				$(".forceAtlas").show();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".labelAdjust").hide();
+				$(".noverLap").hide();
+				$(".openOrd").hide();
+				$(".randomLayout").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="fruchtermanReigngold"){
+				$(".fruchtermanReigngold").show();
+				$(".forceAtlas").hide();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+				$(".labelAdjust").hide();
+				$(".noverLap").hide();
+				$(".openOrd").hide();
+				$(".randomLayout").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="labelAdjust"){
+				$(".labelAdjust").show();
+				$(".fruchtermanReigngold").hide();
+				$(".forceAtlas").hide();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+				$(".noverLap").hide();
+				$(".openOrd").hide();
+				$(".randomLayout").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="noverLap"){
+				$(".noverLap").show();
+				$(".labelAdjust").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".forceAtlas").hide();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+				$(".openOrd").hide();
+				$(".randomLayout").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="openOrd"){
+				$(".openOrd").show();
+				$(".noverLap").hide();
+				$(".labelAdjust").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".forceAtlas").hide();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+				$(".randomLayout").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="randomLayout"){
+				$(".randomLayout").show();
+				$(".openOrd").hide();
+				$(".noverLap").hide();
+				$(".labelAdjust").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".forceAtlas").hide();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+				$(".yifanHu").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="yifanHu"){
+				$(".yifanHu").show();
+				$(".randomLayout").hide();
+				$(".openOrd").hide();
+				$(".noverLap").hide();
+				$(".labelAdjust").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".forceAtlas").hide();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+				$(".yifanhuProportional").hide();
+            }
+			else if($(this).attr("value")=="yifanhuProportional"){
+				$(".yifanhuProportional").show();
+				$(".yifanHu").hide();
+				$(".randomLayout").hide();
+				$(".openOrd").hide();
+				$(".noverLap").hide();
+				$(".labelAdjust").hide();
+				$(".fruchtermanReigngold").hide();
+				$(".forceAtlas").hide();
+				$(".Rotate").hide();
+				$(".Scale").hide();
+            }
+			
+            else{
+                //$(".box").hide();
+				console.log("No value is selected");
+            }
+        });
+    }).change();
+	
+	//Jquery for custom Lightbox NNN ////
+			
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+	        e.preventDefault();
+	    });
+});  // End of document ready function .
---
+++ assets/js/script.js
@@ -0,0 +1,332 @@
+$( document ).ready(function() {
+
+sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+var k,
+neighbors = {},
+index = this.allNeighborsIndex[nodeId] || {};
+for (k in index)
+  neighbors[k] = this.nodesIndex[k];
+  return neighbors;
+});
+
+
+/*
+* GLOBAL variables/settings
+*/
+var sigmaSettings = '{ "eventsEnabled": true, "doubleClickEnabled": false, "enableEdgeHovering": true, "singleHover": true, "edgeHoverColor" : "edge", "edgeHoverColor": "default", "defaultEdgeHoverColor": "#777", "edgeHoverSizeRatio": 10, "edgeColor": "default", "defaultHoverLabelBGColor": "#fff", "defaultEdgeColor": "rgb(205, 220, 213)", "minEdgeSize": 0.2, "labelThreshold": 3, "defaultLabelColor": "#000", "animationsTime": 1000, "borderSize": 2, "outerBorderSize": 3, "defaultNodeOuterBorderColor": "rgb(72,227,236)", "edgeHoverHighlightNodes": "circle", "sideMargin": 10, "edgeHoverExtremities": true, "scalingMode": "outside", "enableCamera": true }';
+var Gsetting = JSON.parse(sigmaSettings);
+var statistics_btn;
+
+
+/*
+*
+*Layouts Submit Operations
+*/
+$(".layout_form").submit(function (e) {
+    e.preventDefault();
+    requestAjax ("http://localhost:9090/layout", $("#" + this.id).serialize(), graphJsonHandler);
+});
+
+/*
+*
+*Statistics Submit Operations
+*/
+$(".statistics_form").submit(function (e) {
+    e.preventDefault();
+//var temp = $("#" + this.id).serialize();
+ //  alert(temp);
+    statistics_btn = $(this).find("input[type=submit]").attr('id');
+    requestAjax ("http://localhost:9090/statistics", $("#" + this.id).serialize(), graphStatisticsHandler);
+});
+
+
+/*
+*
+*Load Test graph
+*/
+requestAjax ("http://localhost:9090/ajax", {}, graphJsonHandler);
+
+
+/*
+* @retrun ajax response
+* Usage: sends ajax request to given URL
+*/
+function requestAjax (ajaxURL, formData, callBackFun) {
+	/*<![CDATA[*/
+   $.ajax({
+      type: "get", url: ajaxURL, data:formData,
+      async : true, beforeSend: function(xhr) {},
+      //on successfull ajax request
+      success: function (graphData) {
+        callBackFun(graphData, formData);
+      },
+      //on error in ajax request
+      error: function(a, b, c){
+      	alert ('Error requested graph Operation.');
+        return false;
+      }
+   });
+  /*]]>*/
+}
+
+
+/*
+* @retrun none
+* Usage: Statistics callback
+*/
+function graphStatisticsHandler (statsData, formData){  
+  var statistics_id = formData.split('=')[1]; //get hidden field ID
+
+  $("#chartContainer").empty();
+  var targeted_popup_class = $("#"+statistics_btn).attr('data-popup-open');
+  $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+  switch (statistics_id) {
+    case "averageDegree":
+      $('span#canvasTitle').text("Average Degree");
+      $("#chartContainer").append('<h3 style="margin: 20px 0 10px 0; color:#bababa">Results:</h3>');
+      averageDegree(statsData);
+    break;
+
+    case "averageWeightedDegree":
+      $('span#canvasTitle').text("Average Weighted Degree");
+      $("#chartContainer").append('<h3 style="margin: 20px 0 10px 0; color:#bababa">Results:</h3>');
+      averageDegree(statsData);
+    break;
+    case "graphDensity":
+      $('span#canvasTitle').text("Graph Density");
+      $("#chartContainer").append('<h3 style="margin: 20px 0 10px 0; color:#bababa">Results:</h3>');
+      graphDensity (statsData);
+    break;
+    case "Modularity":
+      $('span#canvasTitle').text("Modularity");
+      $("#chartContainer").append('<h3 style="margin: 20px 0 10px 0; color:#bababa">Parameters:</h3>');
+      $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Randomize: On</h6>');
+      $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Use edge weights: On</h6>');
+      $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Resolution: 1.0</h6>');
+      modularityClass (statsData);
+    break;
+    case "pageRank":
+      $('span#canvasTitle').text("PageRank");
+      $("#chartContainer").append('<h3 style="margin: 20px 0 10px 0; color:#bababa">Parameters:</h3>');
+      $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Epsilon = 0.001</h6>');
+      $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Probability = 0.85</h6>');
+      pageRank (statsData);
+    break;
+
+    case "connectedComponents":
+      $('span#canvasTitle').text("Connected Components");
+      $("#chartContainer").append('<h3 style="margin: 20px 0 10px 0; color:#bababa">Results:</h3>');
+      connectedComponents (statsData);
+    break;
+    case "avgClusterCoeficients":
+      $('span#canvasTitle').text("Avg. Cluster Coeficients");
+      // $("#chartContainer").append('<h3 style="margin: 20px 0 10px 0; color:#bababa">Parameters:</h3>');
+      // $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Epsilon = 0.001</h6>');
+      // $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Probability = 0.85</h6>');
+//      pageRank (statsData);
+    break;
+  }
+}
+
+function connectedComponents (statsData) {
+  var jsonParsed = $.parseJSON(statsData);
+  $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Number of Weakly Connected Components: '+jsonParsed.weekly_connected_components+'</h6>');
+  $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">Number of Stronlgy Connected Components: '+jsonParsed.strongly_connected_components+'</h6>');
+  //modularity graph
+  $("#chartContainer").append('<div id="connectedcomponents_graph_canvas" style="height: 300px; min-width: 100% !important; margin: 20px 0 20px 0;"></div>');
+  var graphPoint = getSortedGraphPoints(jsonParsed.connected_components);
+  canvasGraph("connectedcomponents_graph_canvas", "Connected Components Report", graphPoint, "Value", "Count");  
+}
+
+function pageRank (statsData) {
+  var jsonParsed = $.parseJSON(statsData);
+  //modularity graph
+  $("#chartContainer").append('<div id="pagerank_graph_canvas" style="height: 300px; min-width: 100% !important; margin: 20px 0 20px 0;"></div>');
+  var graphPoint = getSortedGraphPoints(jsonParsed.pageranks);
+  canvasGraph("pagerank_graph_canvas", "PageRank Distribution", graphPoint, "Value", "Count");  
+}
+
+function modularityClass (statsData) {
+  var jsonParsed = $.parseJSON(statsData);
+  //modularity graph
+  $("#chartContainer").append('<div id="modularity_graph_canvas" style="height: 300px; min-width: 100% !important; margin: 20px 0 20px 0;"></div>');
+  var graphPoint = getSortedGraphPoints(jsonParsed.modularity_class);
+  canvasGraph("modularity_graph_canvas", "Modularity", graphPoint, "Value", "Count");
+}
+
+function graphDensity (statsData) {
+  var jsonParsed = $.parseJSON(statsData);
+  $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">'+jsonParsed.density+'</h6>');
+}
+
+
+function averageDegree (statsData) {
+
+  var jsonParsed = $.parseJSON(statsData);
+  $("#chartContainer").append('<h6 style="margin: 0px 0 20px 0; color:#bababa">'+jsonParsed.avgdegree+'</h6>');
+
+  //degree graph
+  $("#chartContainer").append('<div id="degree_graph_canvas" style="height: 300px; min-width: 100% !important; margin: 20px 0 20px 0;"></div>');
+  var graphPoint = getSortedGraphPoints(jsonParsed.degree);
+  canvasGraph("degree_graph_canvas", "Degree Distribution", graphPoint, "Value", "Count");
+
+  //In-degree graph
+  $("#chartContainer").append('<div id="indegree_graph_canvas" style="height: 300px; min-width: 100% !important; margin: 20px 0 20px 0;"></div>');
+  var graphPoint = getSortedGraphPoints(jsonParsed.indegree);
+  canvasGraph("indegree_graph_canvas", "In-Degree Distribution", graphPoint, "Value", "Count");
+
+  //Out-degree graph
+  $("#chartContainer").append('<div id="outdegree_graph_canvas" style="height: 300px; min-width: 100% !important; margin: 20px 0 20px 0;"></div>');
+  var graphPoint = getSortedGraphPoints(jsonParsed.outdegree);
+  canvasGraph("outdegree_graph_canvas", "Out-Degree Distribution", graphPoint, "Value", "Count");
+}
+
+
+function getSortedGraphPoints (jsonParsedData) {
+  var keysUnsorted = [];
+  $.each(jsonParsedData, function(k, v) {
+   keysUnsorted.push(parseFloat(k));
+  });
+  keysUnsorted.sort(function(a, b){return a-b});
+
+  var graphPoint = [];
+  for (i = 0 ; i < keysUnsorted.length; i++){
+    $.each(jsonParsedData, function(k, v) {
+      if (keysUnsorted[i] == parseFloat(k)) {
+        graphPoint.push({ x: keysUnsorted[i], y: jsonParsedData[k], color: "#ff0000"});
+      }
+    });
+  }
+  return graphPoint;
+}
+
+function canvasGraph (container, gtitle, data, xLabel, yLabel){
+  var options = {
+    title: {
+      text: gtitle+""
+    },
+    axisY:{ 
+        title: yLabel+"",
+    },
+    axisX: {
+        title: xLabel+"",
+    },
+    animationEnabled: true,
+    data: [
+    {
+      type: "spline", //change it to line, area, column, pie, etc
+      dataPoints: data
+    }
+    ]
+  };
+  $("#"+container).CanvasJSChart(options);
+}
+
+/*
+* @retrun none
+* Usage: Forward parsed graph data received from server to Showgraph
+*/
+function graphJsonHandler (graphData){
+  $("#graphLoaderImg").css("display", "none");
+  nodesObject = JSON.parse(graphData);
+  var nodesCount = nodesObject.nodes.nodes.length;
+  if(nodesCount > 0){
+      showGraph(nodesObject.nodes, document.getElementById('container'), Gsetting);
+  } else {
+    alert ('no graph found data found');
+  }
+}
+
+function showGraph(givenData, givenContainer, givenSettings){
+    $('#container').find("canvas").remove();      
+//    givenContainer.innerHTML = "";
+    var s = new sigma( {
+            graph : givenData,
+            renderer: {
+              container:givenContainer,
+              type: 'canvas'
+            },
+            settings:givenSettings
+          });
+
+     totalEdges = s.graph.edges();
+
+   for (var i in totalEdges) {
+     totalEdges[i].type = 'curve';
+   }
+
+   s.graph.nodes().forEach(function(n) {
+      n.originalColor = n.color;
+      n.originalLabel = n.label;
+   });
+
+   s.graph.edges().forEach(function(e) {
+     e.originalColor = e.color;
+   });
+
+   s.bind('clickEdge rightClickEdge', function(e) {
+     console.log(e);
+   });
+
+  //  s.bind('overNode', function(e){
+
+  //     $("#nodeInfoTable").empty();
+  //     //papulate table
+  //     var attr = e.data.node.attributes;
+  //     var row = "<tr><td>" + 'ID:' + "</td><td>" + e.data.node.id; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'Label:' + "</td><td>" + e.data.node.label; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'OriginalLabel:' + "</td><td>" +  e.data.node.originalLabel; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'Color:' + "</td><td>" + e.data.node.color; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'B/w Centrality:' + "</td><td>" + attr["Betweenness Centrality"]; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'Closeness Centrality:' + "</td><td>" + attr["Closeness Centrality"]; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'PageRank:' + "</td><td>" + attr["PageRank"]; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'NeighborCount:' + "</td><td>" + attr["NeighborCount"]; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+  //     var row = "<tr><td>" + 'Eccentricity:' + "</td><td>" + attr["Eccentricity"]; + "</td></tr>";
+  //     $('#nodeInfoTable').append(row);
+
+  //     var nodeId = e.data.node.id;
+  //     var toKeep = s.graph.neighbors(nodeId);
+  //     toKeep[nodeId] = e.data.node;
+
+  //     s.graph.nodes().forEach(function(n) {
+
+  //       if (toKeep[n.id]){
+  //         n.color = n.originalColor;
+  //         n.label = n.originalLabel;
+  //       } else{
+  //         n.color = '#cccccc';
+  //         n.label = "";
+  //       }
+  //   });
+
+  //   s.graph.edges().forEach(function(e) {
+  //     if (toKeep[e.source] && toKeep[e.target])
+  //       e.color ='green';
+  //     else
+  //      e.color = e.originalColor;
+  //   });
+  //   s.refresh();
+  // });
+
+  s.refresh();
+   }
+
+
+
+function waitUntilValChange(variable, value) {
+    if(value === variable) {
+        setTimeout(waitUntilValChange(variable, value), 50);
+        return;
+    }
+}
+
+});
