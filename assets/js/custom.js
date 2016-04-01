b16df51b310327f9099087389dbee4e73471bcf6
revoltn <noman297@gmail.com>
2016-04-01 15:28:51 +0500

    popup for elastic search and upload file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,35 +29,6 @@ $( document ).ready(function() {
 						$( this ).parent().children( 'ul' ).slideToggle( 'fast' );
 				});
 
-	
-
-    $("#selectdata").change(function(){
-        $(this).find("option:selected").each(function(){
-            if($(this).attr("value")=="config_pop_database"){
-            	//alert('this is datasource2');
-            	$(".setting-text").attr('data-popup-open','popup-config-database');
-            	
-            	//$("#selectdataSelectBoxItText").attr('data-popup-open','popup-database');
-            	//$(".pop-custom").show();
-            	//console.log($(this).attr('data-popup-open'));
-            	$(".setting-text" ).click( function( ){
-            		$(".pop-custom").css("display","block");
-            		$(".pop-custom1").css("display","none");
-            		//alert('teststs');
-            	});
-
-            }
-            else if ($(this).attr("value")=="datasource2") {
-            	$(".setting-text").attr('data-popup-open','popup-config');
-            	$(".setting-text" ).click( function( ){
-            		$(".pop-custom1").css("display","block");
-            		$(".pop-custom").css("display","none");
-            		//alert('teststs');
-            	});
-            }
-    	});
-	});
-	
 	//select box 
 	$("#selectdata").selectBoxIt();
 	$("#savedb_id").selectBoxIt();
@@ -69,7 +40,8 @@ $( document ).ready(function() {
 	$("#select-depth_neighbor").selectBoxIt();
     // Easy tree
 	$('#networkoperations-folder').easytree();
-	
+	//Example 1
+   
 	// Nicescroll to whole page
 	$("html").niceScroll({
 		touchbehavior:false,
@@ -280,7 +252,169 @@ $(".degree-selectm #selectdeg").change(function(){
             }
         });
     }).change();
-	
+
+/**********************jquery for datasource popups*****************************************/
+    $("#selectdata").change(function(){
+        $(this).find("option:selected").each(function(){
+            if($(this).attr("value")=="config_pop_database"){
+            	//alert('this is datasource2');
+            	$(".setting-text").attr('data-popup-open','popup-config-database');
+            	
+            	//$("#selectdataSelectBoxItText").attr('data-popup-open','popup-database');
+            	//$(".pop-custom").show();
+            	//console.log($(this).attr('data-popup-open'));
+            	$(".setting-text" ).click( function( ){
+            		$(".pop-custom-db").css("display","block");
+            		$(".pop-custom-es").css("display","none");
+            		$(".pop-custom-file").css("display","none");
+            		//alert('teststs');
+            	});
+
+            }
+            else if ($(this).attr("value")=="config_pop_elasticsearch") {
+            	$(".setting-text").attr('data-popup-open','popup-config-es');
+            	$(".setting-text" ).click( function( ){
+            		$(".pop-custom-db").css("display","none");
+            		$(".pop-custom-es").css("display","block");
+            		$(".pop-custom-file").css("display","none");
+            		//alert('teststs');
+            	});
+            }
+            else if ($(this).attr("value")=="config_file_upload") {
+            	$(".setting-text").attr('data-popup-open','popup-config-file');
+            	$(".setting-text" ).click( function( ){
+            		$(".pop-custom-file").css("display","block");
+            		$(".pop-custom-db").css("display","none");
+            		$(".pop-custom-es").css("display","none");
+            		//alert('teststs');
+            		  
+            	});
+            }
+    	});
+	});
+	 /***********************file upload popup start****************/
+    $("#filer_input").filer({
+        limit: 1,
+        maxSize: null,
+        extensions: null,
+        changeInput: '<div class="jFiler-input-dragDrop"><div class="jFiler-input-inner"><div class="jFiler-input-icon"><i class="icon-jfi-cloud-up-o"></i></div><div class="jFiler-input-text"><h3>Drag&Drop files here</h3> <span style="display:inline-block; margin: 15px 0">or</span></div><a class="jFiler-input-choose-btn blue">Browse Files</a></div></div>',
+        showThumbs: true,
+        theme: "dragdropbox",
+        templates: {
+            box: '<ul class="jFiler-items-list jFiler-items-grid"></ul>',
+            item: '<li class="jFiler-item">\
+                        <div class="jFiler-item-container">\
+                            <div class="jFiler-item-inner">\
+                                <div class="jFiler-item-thumb">\
+                                    <div class="jFiler-item-status"></div>\
+                                    <div class="jFiler-item-info">\
+                                        <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                    </div>\
+                                    {{fi-image}}\
+                                </div>\
+                                <div class="jFiler-item-assets jFiler-row">\
+                                    <ul class="list-inline pull-left">\
+                                        <li>{{fi-progressBar}}</li>\
+                                    </ul>\
+                                    <ul class="list-inline pull-right">\
+                                        <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </li>',
+            itemAppend: '<li class="jFiler-item">\
+                            <div class="jFiler-item-container">\
+                                <div class="jFiler-item-inner">\
+                                    <div class="jFiler-item-thumb">\
+                                        <div class="jFiler-item-status"></div>\
+                                        <div class="jFiler-item-info">\
+                                            <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                            <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                        </div>\
+                                        {{fi-image}}\
+                                    </div>\
+                                    <div class="jFiler-item-assets jFiler-row">\
+                                        <ul class="list-inline pull-left">\
+                                            <li><span class="jFiler-item-others">{{fi-icon}}</span></li>\
+                                        </ul>\
+                                        <ul class="list-inline pull-right">\
+                                            <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                        </ul>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </li>',
+            progressBar: '<div class="bar"></div>',
+            itemAppendToEnd: false,
+            removeConfirmation: true,
+            _selectors: {
+                list: '.jFiler-items-list',
+                item: '.jFiler-item',
+                progressBar: '.bar',
+                remove: '.jFiler-item-trash-action'
+            }
+        },
+        dragDrop: {
+            dragEnter: null,
+            dragLeave: null,
+            drop: null,
+        },
+        uploadFile: {
+            url: "./php/upload.php",
+            data: null,
+            type: 'POST',
+            enctype: 'multipart/form-data',
+            beforeSend: function(){},
+            success: function(data, el){
+                var parent = el.find(".jFiler-jProgressBar").parent();
+                el.find(".jFiler-jProgressBar").fadeOut("slow", function(){
+                    $("<div class=\"jFiler-item-others text-success\"><i class=\"icon-jfi-check-circle\"></i> Success</div>").hide().appendTo(parent).fadeIn("slow");    
+                });
+            },
+            error: function(el){
+                var parent = el.find(".jFiler-jProgressBar").parent();
+                el.find(".jFiler-jProgressBar").fadeOut("slow", function(){
+                    //$("<div class=\"jFiler-item-others text-error\"><i class=\"icon-jfi-minus-circle\"></i> Error</div>").hide().appendTo(parent).fadeIn("slow");    
+                });
+            },
+            statusCode: null,
+            onProgress: null,
+            onComplete: null
+        },
+        files: null,
+        addMore: false,
+        clipBoardPaste: true,
+        excludeName: null,
+        beforeRender: null,
+        afterRender: null,
+        beforeShow: null,
+        beforeSelect: null,
+        onSelect: null,
+        afterShow: null,
+        onRemove: function(itemEl, file, id, listEl, boxEl, newInputEl, inputEl){
+            var file = file.name;
+            $.post('./php/remove_file.php', {file: file});
+        },
+        onEmpty: null,
+        options: null,
+        captions: {
+            button: "Choose Files",
+            feedback: "Choose files To Upload",
+            feedback2: "files were chosen",
+            drop: "Drop file here to Upload",
+            removeConfirmation: "Are you sure you want to remove this file?",
+            errors: {
+                filesLimit: "Only {{fi-limit}} files are allowed to be uploaded.",
+                filesType: "Only Images are allowed to be uploaded.",
+                filesSize: "{{fi-name}} is too large! Please upload file up to {{fi-maxSize}} MB.",
+                filesSizeAll: "Files you've choosed are too large! Please upload files up to {{fi-maxSize}} MB."
+            }
+        }
+    });
+
+	 /***********************file upload popup end****************/
 	//Jquery for custom Lightbox NNN ////
 			
     $('[data-popup-close]').on('click', function(e)  {
